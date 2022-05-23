a5046b28f306da93d06208a913759bad84cc5a6f
mahmoudabuelnaga <mahmoudaboelnaga392@gmail.com>
2022-05-23 22:40:19 +0200

    Finsh Building a Blog Application

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views import generic
 # Create your views here.
+class PostListView(generic.ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 1
+    template_name = "blog/post/list.html"
+
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 1)
+#     page = request.GET.get('page')
+    
+#     try:
+#         posts = paginator.page(page)
+#         print(posts)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+
+#     context = {
+#         'posts':posts,
+#     }
+#     return render(request, 'blog/post/list.html', context)
+
+
+class PostDetailView(generic.DeleteView):
+    queryset = Post.objects.all()
+    context_object_name = 'post'
+    template_name = 'blog/post/detail.html'
+
+# def post_detail(request, year, month, day, slug):
+#     post = get_object_or_404(
+#         Post, slug=slug, status='published', 
+#         publish__year=year, publish__month=month, publish__day=day
+#     )
+
+#     context = {
+#         'post':post,
+#     }
+
+#     return render(request, 'blog/post/detail.html', context)
